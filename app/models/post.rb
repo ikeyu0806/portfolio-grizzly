1c3f5de8854c3970ac60f2ebdfd2c5f85ff1a999
yuki.ikegaya <git config --global user.email>
2018-12-13 18:02:43 +0900

    新着記事の表示数を10に設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+  MAX_DISPLAY = 10
+
   validates :title, presence: true
   validates :content, presence: true
 
-  scope :load_recent_posts, -> { all.order(created_at: "DESC").limit(10) }
+  scope :load_recent_posts, -> { all.order(created_at: "DESC").limit(MAX_DISPLAY) }
 end
