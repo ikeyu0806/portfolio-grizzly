3f7163a65360db3eecd636bdaca0f2ca40debccd
yuki.ikegaya <git config --global user.email>
2018-12-13 17:19:23 +0900

    コントローラのrspecから不要な箇所削除

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -45,12 +45,11 @@ RSpec.describe PostsController, type: :controller do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # PostsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
 
   describe "GET #index" do
     it "returns a success response" do
       Post.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index, params: {}
       expect(response).to be_successful
     end
   end
@@ -58,14 +57,14 @@ RSpec.describe PostsController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       post = Post.create! valid_attributes
-      get :show, params: {id: post.to_param}, session: valid_session
+      get :show, params: {id: post.to_param}
       expect(response).to be_successful
     end
   end
 
   describe "GET #new" do
     it "returns a success response" do
-      get :new, params: {}, session: valid_session
+      get :new, params: {}
       expect(response).to be_successful
     end
   end
@@ -73,7 +72,7 @@ RSpec.describe PostsController, type: :controller do
   describe "GET #edit" do
     it "returns a success response" do
       post = Post.create! valid_attributes
-      get :edit, params: {id: post.to_param}, session: valid_session
+      get :edit, params: {id: post.to_param}
       expect(response).to be_successful
     end
   end
@@ -82,19 +81,19 @@ RSpec.describe PostsController, type: :controller do
     context "with valid params" do
       it "creates a new Post" do
         expect {
-          post :create, params: {post: valid_attributes}, session: valid_session
+          post :create, params: {post: valid_attributes}
         }.to change(Post, :count).by(1)
       end
 
       it "redirects to the created post" do
-        post :create, params: {post: valid_attributes}, session: valid_session
+        post :create, params: {post: valid_attributes}
         expect(response).to redirect_to(Post.last)
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {post: invalid_attributes}, session: valid_session
+        post :create, params: {post: invalid_attributes}
         expect(response).to be_successful
       end
     end
@@ -111,14 +110,14 @@ RSpec.describe PostsController, type: :controller do
 
       it "updates the requested post" do
         post = Post.create! valid_attributes
-        put :update, params: {id: post.to_param, post: new_attributes}, session: valid_session
+        put :update, params: {id: post.to_param, post: new_attributes}
         post.reload
         expect(response).to be_successful
       end
 
       it "redirects to the post" do
         post = Post.create! valid_attributes
-        put :update, params: {id: post.to_param, post: valid_attributes}, session: valid_session
+        put :update, params: {id: post.to_param, post: valid_attributes}
         expect(response).to redirect_to(post)
       end
     end
@@ -126,7 +125,7 @@ RSpec.describe PostsController, type: :controller do
     context "無効なパラメータを投稿した時" do
       it "returns a success response (i.e. to display the 'edit' template)" do
         post = Post.create! valid_attributes
-        put :update, params: {id: post.to_param, post: invalid_attributes}, session: valid_session
+        put :update, params: {id: post.to_param, post: invalid_attributes}
         expect(response).to be_successful
       end
     end
@@ -136,13 +135,13 @@ RSpec.describe PostsController, type: :controller do
     it "destroys the requested post" do
       post = Post.create! valid_attributes
       expect {
-        delete :destroy, params: {id: post.to_param}, session: valid_session
+        delete :destroy, params: {id: post.to_param}
       }.to change(Post, :count).by(-1)
     end
 
     it "redirects to the posts list" do
       post = Post.create! valid_attributes
-      delete :destroy, params: {id: post.to_param}, session: valid_session
+      delete :destroy, params: {id: post.to_param}
       expect(response).to redirect_to(posts_url)
     end
   end
