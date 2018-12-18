5e6f288f89388ed0b006a966a3c73a0025e8a85c
yuki.ikegaya <git config --global user.email>
2018-12-18 15:53:09 +0900

    user/nameにカレントユーザの記事一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    user = User.find_by(name: params[:name])
-    @posts = user.posts
+    @posts = Post.where(user_id: current_user.id)
   end
 end
