2abdb3cb23c34809271f740551b8a6583de26946
yuki.ikegaya <git config --global user.email>
2018-12-19 16:59:12 +0900

    relationテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_052235) do
+ActiveRecord::Schema.define(version: 2018_12_19_074853) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_12_18_052235) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "follow_id"
+    t.bigint "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id", "follower_id"], name: "index_relations_on_follow_id_and_follower_id", unique: true
+    t.index ["follower_id"], name: "fk_rails_2929a0fe05"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -36,4 +45,6 @@ ActiveRecord::Schema.define(version: 2018_12_18_052235) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "relations", "users", column: "follow_id"
+  add_foreign_key "relations", "users", column: "follower_id"
 end
---
+++ spec/factories/relations.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :relation do
+    follow_id { "" }
+    follower_id { "" }
+  end
+end
