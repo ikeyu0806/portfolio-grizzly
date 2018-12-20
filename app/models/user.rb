d032f17ce8a268711f325e454aea5801b33449e3
yuki.ikegaya <git config --global user.email>
2018-12-20 11:59:58 +0900

    ユーザモデルの関連付け修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
 
-  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
+  has_many :passive_relationships, class_name:  "Relation",
+                                   foreign_key: "follow_id",
                                    dependent:   :destroy
 
   has_many :following, through: :active_relationships, source: :followed
