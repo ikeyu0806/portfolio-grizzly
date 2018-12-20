8ae56b521662eccdccc049fb622f9bec7bbb844f
yuki.ikegaya <git config --global user.email>
2018-12-20 15:08:55 +0900

    ユーザ一覧ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
 
   def show
     @posts = Post.where(user_id: current_user.id)
