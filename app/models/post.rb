8e4f1d5200d6c229943b07fa2ac9d11a6981bb46
yuki.ikegaya <git config --global user.email>
2018-12-20 14:30:40 +0900

    カテゴリとポスト間にdependent追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent:   :destroy
   has_many :comments, dependent:   :destroy
 
-  has_many :post_categories
+  has_many :post_categories, dependent:   :destroy
   has_many :categories, through: :post_categories
 
   scope :recent_posts,
