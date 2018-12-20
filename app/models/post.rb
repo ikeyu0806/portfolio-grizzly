4cc8e1830c7405f4da87090ebaac775a9926287f
yuki.ikegaya <git config --global user.email>
2018-12-20 13:26:52 +0900

    likeモデルの関連付け作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  has_many :likes, dependent:   :destroy
+
   scope :recent_posts,
         -> { order(created_at: :desc).limit(MAX_DISPLAY) }
 end
