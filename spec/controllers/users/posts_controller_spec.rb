82d046bdad3f954285c1381c55306877590d0381
yuki.ikegaya <git config --global user.email>
2018-12-21 15:34:13 +0900

    コントローラのテスト修正

--- spec/controllers/users/posts_controller_spec.rb
+++ spec/controllers/users/posts_controller_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe Users::PostsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:post) { create(:post, user: user) }
 
-  before do
-    sign_in user
-  end
-
   describe 'GET #index' do
     it 'returns a success response' do
-      get :index, params: { name: user.to_param }
+      get :index, params: { user_name: user.to_param }
       expect(response).to be_successful
     end
   end
