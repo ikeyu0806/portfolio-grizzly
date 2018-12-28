3ec88915123e571727841486f92c6aeea4a6939f
yuki.ikegaya <git config --global user.email>
2018-12-28 16:41:29 +0900

    relations_controllerのテスト追加

--- spec/controllers/relations_controller_spec.rb
+++ spec/controllers/relations_controller_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe RelationsController, type: :controller do
+  let!(:user1) { create(:user1) }
+  let!(:user2) { create(:user2) }
+  let(:create_params) do
+    {
+      relation: {
+        follow_id: 2
+      }
+    }
+  end
+
+  before do
+    sign_in user1
+  end
+
   describe 'GET #create' do
     it 'returns http success' do
-      get :create
-      expect(response).to have_http_status(:success)
+      expect do
+        post :create, params: create_params
+      end.to change(Relation, :count).by(1)
     end
   end
 
   describe 'GET #destroy' do
+    let!(:relation) { create(:relation) }
     it 'returns http success' do
-      get :destroy
-      expect(response).to have_http_status(:success)
+      expect do
+        delete :destroy, params: { id: relation.to_param }
+      end.to change(Relation, :count).by(-1)
     end
   end
 end
