9d380e99eeaba4ae1f035cf28966290612fedf4e
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-08 15:36:29 +0900

    ビュー、コントローラの作成

---
+++ app/controllers/users/relation_lists_controller.rb
@@ -0,0 +1,8 @@
+module Users
+  class RelationListsController < ApplicationController
+    def index
+      # @followed_users =
+      # @following_users
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # resources :users, only: %i(index show), param: :name
   resources :users, only: %i(index show), param: :name do
     resources :posts, only: %i(index), controller: 'users/posts'
+    resources　:relation_lists, only: %i(index), controller: 'users/relation_lists'
   end
 
   resource :setting do
