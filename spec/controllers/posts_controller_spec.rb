1d6249ce62f6389891a62da03587a4e416e8e21d
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-15 12:58:37 +0900

    テストケースの記述修正

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -19,12 +19,8 @@ RSpec.describe PostsController, type: :controller do
     }
   end
 
-  # before do
-  #   sign_in user
-  # end
-
   describe 'GET #index' do
-    it 'returns a success response' do
+    it '正常に応答すること' do
       Post.create! valid_attributes
       get :index, params: {}
       expect(response).to be_successful
@@ -32,7 +28,7 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe 'GET #show' do
-    it 'returns a success response' do
+    it '正常に応答すること' do
       post = Post.create! valid_attributes
       get :show, params: { id: post.to_param }
       expect(response).to be_successful
@@ -70,7 +66,7 @@ RSpec.describe PostsController, type: :controller do
       end
     end
     context 'ログインページに遷移すること' do
-      it 'returns a success response' do
+      it '正常に応答すること' do
         post = Post.create! valid_attributes
         get :edit, params: { id: post.to_param }
         expect(response).to redirect_to new_user_session_path
@@ -83,21 +79,21 @@ RSpec.describe PostsController, type: :controller do
       before do
         sign_in user
       end
-      context 'with valid params' do
-        it 'creates a new Post' do
+      context '正常なパラメータの場合' do
+        it '記事を作成する時' do
           expect do
             post :create, params: { post: valid_attributes }
           end.to change(Post, :count).by(1)
         end
 
-        it 'redirects to the created post' do
+        it '記事作成ページにリダイレクトすること' do
           post :create, params: { post: valid_attributes }
           expect(response).to redirect_to(Post.last)
         end
       end
 
-      context 'with invalid params' do
-        it "returns a success response (i.e. to display the 'new' template)" do
+      context '不正なパラメータの場合' do
+        it "正常に応答すること" do
           post :create, params: { post: invalid_attributes }
           expect(response).to be_successful
         end
@@ -105,25 +101,10 @@ RSpec.describe PostsController, type: :controller do
     end
 
     context 'ログインしていない場合' do
-      context 'with valid params' do
-        # it 'creates a new Post' do
-        #   expect do
-        #     post :create, params: { post: valid_attributes }
-        #   end.to change(Post, :count).by(1)
-        # end
-
-        it 'redirects to the created post' do
-          post :create, params: { post: valid_attributes }
-          expect(response).to redirect_to new_user_session_path
-        end
+      it 'ログインページに遷移すること' do
+        post :create, params: { post: valid_attributes }
+        expect(response).to redirect_to new_user_session_path
       end
-
-      # context 'with invalid params' do
-      #   it "returns a success response (i.e. to display the 'new' template)" do
-      #     post :create, params: { post: invalid_attributes }
-      #     expect(response).to be_successful
-      #   end
-      # end
     end
   end
 
@@ -132,26 +113,26 @@ RSpec.describe PostsController, type: :controller do
       before do
         sign_in user
       end
-      context 'with valid params' do
+      context '正常なパラメータの場合' do
         let(:new_attributes) do
           {
             title: 'new_title',
             content: 'new_content'
           }
         end
-        it 'updates the requested post' do
+        it '想定通りに更新されること' do
           post = Post.create! valid_attributes
           put :update, params: { id: post.to_param, post: new_attributes }
           post.reload
           expect(post).to have_attributes(new_attributes)
         end
-        it 'redirects to the post' do
+        it '記事一覧ページに遷移すること' do
           post = Post.create! valid_attributes
           put :update, params: { id: post.to_param, post: valid_attributes }
           expect(response).to redirect_to(post)
         end
       end
-      context 'with invalid params' do
+      context '不正なパラメータの場合' do
         it "returns a success response (i.e. to display the 'edit' template)" do
           post = Post.create! valid_attributes
           put :update, params: { id: post.to_param, post: invalid_attributes }
@@ -168,25 +149,12 @@ RSpec.describe PostsController, type: :controller do
             content: 'new_content'
           }
         end
-        # it 'updates the requested post' do
-        #   post = Post.create! valid_attributes
-        #   put :update, params: { id: post.to_param, post: new_attributes }
-        #   post.reload
-        #   expect(post).to have_attributes(new_attributes)
-        # end
-        it 'redirects to the post' do
+        it 'ログイン画面に遷移すること' do
           post = Post.create! valid_attributes
           put :update, params: { id: post.to_param, post: valid_attributes }
           expect(response).to redirect_to new_user_session_path
         end
       end
-      # context 'with invalid params' do
-      #   it "returns a success response (i.e. to display the 'edit' template)" do
-      #     post = Post.create! valid_attributes
-      #     put :update, params: { id: post.to_param, post: invalid_attributes }
-      #     expect(response).to be_successful
-      #   end
-      # end
     end
   end
 
@@ -195,14 +163,14 @@ RSpec.describe PostsController, type: :controller do
       before do
         sign_in user
       end
-      it 'destroys the requested post' do
+      it '記事が削除されること' do
         post = Post.create! valid_attributes
         expect do
           delete :destroy, params: { id: post.to_param }
         end.to change(Post, :count).by(-1)
       end
 
-      it 'redirects to the posts list' do
+      it '記事一覧ページにリダイレクトすること' do
         post = Post.create! valid_attributes
         delete :destroy, params: { id: post.to_param }
         expect(response).to redirect_to(posts_url)
@@ -210,13 +178,6 @@ RSpec.describe PostsController, type: :controller do
     end
 
     context 'ログインしていない場合' do
-      # it 'destroys the requested post' do
-      #   post = Post.create! valid_attributes
-      #   expect do
-      #     delete :destroy, params: { id: post.to_param }
-      #   end.to change(Post, :count).by(-1)
-      # end
-
       it 'ログインページに遷移すること' do
         post = Post.create! valid_attributes
         delete :destroy, params: { id: post.to_param }
