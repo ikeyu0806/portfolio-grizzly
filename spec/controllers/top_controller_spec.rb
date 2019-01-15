9278452eab7739f8730aea96b51b3a6951437d68
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-15 14:51:19 +0900

    トップ画面のコントローラテストにログイン時と非ログイン時のパターン追加

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -4,18 +4,39 @@ RSpec.describe TopController, type: :controller do
   let!(:user) { create(:user) }
 
   describe 'GET #index' do
-    let!(:created_posts) do
-      FactoryBot.create_list(:post, 11, user: user)
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      let!(:created_posts) do
+        FactoryBot.create_list(:post, 11, user: user)
+      end
+
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+
+      it "表示数が10であること" do
+        get :index
+        expect(assigns(:recent_posts).count).to eq 10
+      end
     end
 
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+    context 'ログインしていない場合' do
+      let!(:created_posts) do
+        FactoryBot.create_list(:post, 11, user: user)
+      end
 
-    it "表示数が10であること" do
-      get :index
-      expect(assigns(:recent_posts).count).to eq 10
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+
+      it "表示数が10であること" do
+        get :index
+        expect(assigns(:recent_posts).count).to eq 10
+      end
     end
   end
 end
