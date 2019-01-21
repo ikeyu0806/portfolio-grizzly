a57614bb5007a6b20ad018c1324de582aee7c4cc
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-21 17:18:03 +0900

    letの即時評価不要なので一応削除

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let!(:post_user) { create(:user1) }
-  let!(:comment_user) { create(:user2) }
-  let!(:other_user) { create(:user3) }
-  let!(:commented_post) { create(:post, user: post_user) }
-  let!(:comment) { create(:comment, user: comment_user, post: commented_post) }
+  let(:post_user) { create(:user1) }
+  let(:comment_user) { create(:user2) }
+  let(:other_user) { create(:user3) }
+  let(:commented_post) { create(:post, user: post_user) }
+  let(:comment) { create(:comment, user: comment_user, post: commented_post) }
 
   it '記事を書いたユーザがコメントを削除できないこと' do
     expect(comment.destroyable?(post_user)).to be true
