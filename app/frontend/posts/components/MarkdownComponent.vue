a50785c5c80f902f17809ca30f59bc058ac97d27
ikeyu0806 <ikeyu0806@gmail.com>
2019-01-31 14:19:14 +0900

    コンポーネントの作成

---
+++ app/frontend/posts/components/MarkdownComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-html="compiledMarkdown"></div>
+  <!-- <p>hello</p> -->
+</template>
+
+<script>
+import marked from 'marked';
+
+export default {
+  // propsを定義
+  // v-bind:propsの名前
+  props: ['value'],
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.value, { sanitize: true })
+    }
+  }
+}
+</script>
--- app/frontend/posts/form.js
+++ app/frontend/posts/form.js
@@ -1,18 +1,24 @@
 import marked from 'marked';
 import _ from 'lodash';
+import MarkdownComponent from './components/MarkdownComponent';
 
 export default {
   // data: {
   //   input: post
   // },
-  computed: {
-    compiledMarkdown: function () {
-      return marked(this.post, { sanitize: true })
-    }
-  },
+
+  // computed: {
+  //   compiledMarkdown: function () {
+  //     return marked(this.post, { sanitize: true })
+  //   }
+  // },
+
   // methods: {
   //   update: _.debounce(function (e) {
   //     this.input = e.target.value
   //   }, 300)
   // }
+  components: {
+    MarkdownComponent
+  }
 }
