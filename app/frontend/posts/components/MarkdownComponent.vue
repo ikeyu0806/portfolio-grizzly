40baddf8bb3ca8ed535c95666cae4d53a02695f2
ikeyu0806 <ikeyu0806@gmail.com>
2019-02-01 16:40:34 +0900

    debounce処理導入

--- app/frontend/posts/components/MarkdownComponent.vue
+++ app/frontend/posts/components/MarkdownComponent.vue
@@ -1,17 +1,31 @@
 <template>
-  <div v-html="compiledMarkdown"></div>
-  <!-- <p>hello</p> -->
+  <div v-html="compiled"></div>
 </template>
 
 <script>
 import marked from 'marked';
+import _ from 'lodash';
 
 export default {
+  data: function (){
+      return {
+        compiled: ''
+      }
+  },
   props: ['value'],
-  computed: {
-    compiledMarkdown: function () {
-      return marked(this.value, { sanitize: true })
+  methods: {
+    compileMarkdown: function () {
+      this.compiled = marked(this.value, { sanitize: true } )
     }
+  },
+  watch: {
+    value: function (newPost, oldPost) {
+      this.debounced()
+    }
+  },
+  created: function() {
+    this.compiled = marked(this.value, { sanitize: true } )
+    this.debounced = _.debounce(this.compileMarkdown, 300)
   }
 }
 </script>
