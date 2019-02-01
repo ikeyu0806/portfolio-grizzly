19d8f2270c1a58547504f53a5ace6d85eb9ed3f6
ikeyu0806 <ikeyu0806@gmail.com>
2019-02-01 15:21:20 +0900

    デザイン修正

--- app/frontend/posts/form.js
+++ app/frontend/posts/form.js
@@ -3,5 +3,10 @@ import MarkdownComponent from './components/MarkdownComponent';
 export default {
   components: {
     MarkdownComponent
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
   }
 }
